2e035539cf8dba18f1a5456c7a01b16cbdb61ad4
H Grasso <aitch.grasso@gmail.com>
2023-02-07 21:17:01 -0600

    2-7 commit

--- class26-materials/class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+let pizza = {}
+
+pizza.brand = "dominos"
+pizza.size = "Lg"
+pizza.toppings = ["pineapple", "canadian bacon"]
+pizza.cheese = "mozz"
+
+pizza.estimatedDelivery = function(){
+    console.log('calculating...')
+}
+
+function MakePizza(pizzaBrand, pizzaSize){
+    
+    this.brand = "dominos"
+    this.size = "Lg"
+    pizza.toppings = ["pineapple", "canadian bacon"]
+    pizza.cheese = "mozz"
+}
