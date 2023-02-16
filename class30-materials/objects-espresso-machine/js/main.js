d7595baf6ec4f53f00239c15179edc17a9bb3e2d
H Grasso <aitch.grasso@gmail.com>
2023-02-15 20:09:33 -0600

    espresso module

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,15 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine{
+    constructor(grindSettings, waterAmount, milkSteamTemp, numberShots){
+        this.grind = grindSettings //peramiters
+        this.water = waterAmount
+        this.milk = milkSteamTemp
+        this.shot = numberShots
+    }
+    burn(){
+        alert("Ow! I got burned by the milk! It's currently at ${EspressoMachine.milk}")
+    }
+    art(){
+        InputEvent:("What type of art do you want?")
+    }
+    }
