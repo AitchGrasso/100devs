381eb8e8666af49712c279edad9ca936a67dbef7
H Grasso <aitch.grasso@gmail.com>
2023-02-20 18:11:37 -0600

    Addition of 'animal' class

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,17 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+class Pig extends Animal{
+    constructor(name, flavor){
+        super(name)
+        this.flavor = flavor
+    }
+}
 
+let wilbur = new Pig('Wilbur', 'Delicious')
