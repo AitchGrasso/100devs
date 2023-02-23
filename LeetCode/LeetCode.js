67ed410f587aff8e1891e4d29a6cb700499e74bb
H Grasso <aitch.grasso@gmail.com>
2023-02-22 18:06:26 -0600

    Leetcode 897

---
+++ LeetCode/LeetCode.js
@@ -0,0 +1,4 @@
+
+//897. Increasing Order Search Tree
+//status: FAIL - What is are nodes? And how do I work with them?
+//URL: https://leetcode.com/problems/increasing-order-search-tree/description/
