4d3175e4e1b6008d2ae17156ccf5641e9e67208d
H Grasso <aitch.grasso@gmail.com>
2023-03-03 16:50:16 -0600

    Isograms

--- codewars/march.js
+++ codewars/march.js
@@ -24,3 +24,26 @@ function getMiddle(s)
 
 console.log(getMiddle('testing'))
 
+// DATE: 3/3
+// NAME: Isograms
+// KYU: 7
+// INSTRUCTION: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+// STATUS: complete! 
+// ISSUE: using .sort
+
+function isIsogram(str){
+  //str > ignore case > to array > sort? loop to find > bool
+  str = str.toLowerCase()
+  let array = str.split("").sort();
+  for (let i = 0; i < array.length; i++){
+    if (array[i] == array[i + 1]){
+      return false
+    }
+  }
+  return true
+}
+
