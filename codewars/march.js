fdb13f4838c1ade4a6e9ee91cf3ac0ef4122358a
H Grasso <aitch.grasso@gmail.com>
2023-03-04 18:56:09 -0600

    Switch Case

--- codewars/march.js
+++ codewars/march.js
@@ -47,3 +47,29 @@ function isIsogram(str){
   return true
 }
 
+// DATE: 3/4
+// NAME: Who likes it?
+// KYU: 6
+// INSTRUCTION: You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+// STATUS: conplete
+// ISSUE: I need to make an anki card on how to use case
+
+function likes(names) {
+  switch(names.length){
+      case 0:
+          return("no one likes this");
+          break;
+      case 1:
+          return(`${names[0]} likes this`)
+          break;
+      case 2:
+          return(`${names[0]} and ${names[1]} like this`)
+          break;
+      case 3:
+          return(`${names[0]}, ${names[1]} and ${names[2]} like this`)
+          break;
+      default:
+          return(`${names[0]}, ${names[1]} and ${names.length - 2} others like this`)
+          break;
+  }
+}
