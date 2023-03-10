3643901dc904c69960c791b012530532c4a25779
H Grasso: PC <aitch.grasso@gmail.com>
2023-03-09 21:52:41 -0600

    Vanilla JS Review

--- super-review/objects-dog-photos/js/main.js
+++ super-review/objects-dog-photos/js/main.js
@@ -1,1 +1,9 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+fetch()
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${}`)
+    });
--- super-review/objects-street-fighter/js/main.js
+++ super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,31 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+class streetfighter() => {
+    constructor makeAFighter(health, powers, hitpoints){
+        this _health = health
+        this _powers = powers
+        this _hitpoints = hitpoints
+
+        function yell(){
+            console.log("AHHHHJJJJ")
+        }
+
+    }
+}
+
+class MakeAFighter {
+    constructor(health, powers, hitpoints) {
+        this._health = health
+        this._powers = powers
+        this._hitpoints = hitpoints
+
+        function yell() {
+            console.log("AHHHHJJJJ")
+        }
+
+        this.throw = function hit() {
+            console.log('taunt')
+        }
+    }
+}
+
+let chunLi = new StreetFighter
