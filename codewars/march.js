08302de8e2908ea38a4dc3f8fdca48f7475f84c2
H Grasso: PC <aitch.grasso@gmail.com>
2023-03-15 15:04:51 -0500

    3-15, two solutions

--- codewars/march.js
+++ codewars/march.js
@@ -130,3 +130,79 @@ function filter_list(l) {
 function filter_list(l) {
   return l.filter(v => typeof v == "number")
  }
+
+// DATE: 3/13
+// NAME: Complementary DNA
+// KYU: 7
+// INSTRUCTION: c>g, A,T
+// STATUS: COMPLETE
+// ISSUE:
+
+function DNAStrand(dna){
+  let newDNA = ""
+  for (let i = 0; i <= dna.length; i++){
+    if (dna[i] == "A"){
+      newDNA = newDNA + "T"
+    }if(dna[i] == "T"){
+      newDNA = newDNA + "A"
+    }if (dna[i] == "C"){
+      newDNA = newDNA + "G"
+    }if (dna[i] == "G"){
+      newDNA = newDNA + "C"
+    }
+  }
+  return newDNA
+}
+
+// Best Practice:
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+  return dna.split('').map(function(v){ return pairs[v] }).join('');
+}
+
+// DATE: 3/14
+// NAME: Find the middle element
+// KYU: 7
+// INSTRUCTION: As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+// STATUS: COMPLETE
+// ISSUE:
+
+// DATE: 3/15
+// NAME: Highest and Lowest
+// KYU: 7
+// INSTRUCTION: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// STATUS: COMPLETE
+// ISSUE:
+
+function highAndLow(numbers){ 
+  let arrayNum = (numbers.split(" "))
+  let numNum = arrayNum.map(ele => Number(ele))
+  numNum = numNum.sort(function(a,b) {
+    return a - b})
+  
+  return (numNum[numNum.length - 1] + " " +  numNum[0])
+}
+
+//again but better:
+
+function highAndLow(numbers){ 
+  let arrayNum = (numbers.split(" ")).map(ele => Number(ele)).sort(function(a,b) {return a - b})
+  return (arrayNum[arrayNum.length - 1 ] + " " + arrayNum[0])
+}
+
+// DATE: 3/15
+// NAME: Find the odd int
+// KYU: 6
+// INSTRUCTION: Given an array of integers, find the one that appears an odd number of times.There will always be only one integer that appears an odd number of times.
+// STATUS: Completed
+// ISSUE:
+
+function findOdd(A) {
+  for (let i = 0; i < A.length; i++){
+    let aNum = A.filter(ele => ele === A[i])
+    if (aNum.length % 2 !== 0){
+      return A[i]
+    }
+  }
+}
